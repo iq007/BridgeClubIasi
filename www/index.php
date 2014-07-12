8b818356af7760b8c19ee404d74133546c8a88d2
iq007 <a1b2ff@yahoo.com>
2014-07-13 01:36:40 +0300

    Updated Google Map

--- www/index.php
+++ www/index.php
@@ -15,7 +15,8 @@
 		<div id="br_index_info" class="well" style="display: none">
 			<div class="row">
 				<div class="col-md-4">			
-						<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=iasi,+romania&amp;sll=37.0625,-95.677068&amp;sspn=33.764224,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Iasi,+Romania&amp;ll=47.179023,27.568388&amp;spn=0.005105,0.00912&amp;z=16&amp;output=embed"></iframe><br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=iasi,+romania&amp;sll=37.0625,-95.677068&amp;sspn=33.764224,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Iasi,+Romania&amp;ll=47.179023,27.568388&amp;spn=0.005105,0.00912&amp;z=16" style="color:#0000FF;text-align:left">View Larger Map</a></small>
+				<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d1355.9753774855335!2d27.566970635581914!3d47.17840074253418!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6c585cfb415d1b55!2sBridge+Club+Iasi!5e0!3m2!1sen!2s!4v1405202557204" width="400" height="300" frameborder="0" style="border:0"></iframe>
+				<br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=iasi,+romania&amp;sll=37.0625,-95.677068&amp;sspn=33.764224,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Iasi,+Romania&amp;ll=47.179023,27.568388&amp;spn=0.005105,0.00912&amp;z=16" style="color:#0000FF;text-align:left">View Larger Map</a></small>
 				</div>
 				<div class="col-md-4">
 						<img alt="Bridge Club Iasi" class="img-thumbnail" data-src="holder.js/425x350" style="width: 425px; height: 350px;" src="./images/bridgeclub.jpg">
