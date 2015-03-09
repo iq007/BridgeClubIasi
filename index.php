5b7a2432e7a9e486b20395c78eea144c99946da8
iq007 <a1b2ff@yahoo.com>
2015-03-09 12:57:46 +0200

    Removed www

--- index.php
+++ index.php
@@ -19,17 +19,18 @@
 				<br /><small><a href="http://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=iasi,+romania&amp;sll=37.0625,-95.677068&amp;sspn=33.764224,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=Iasi,+Romania&amp;ll=47.179023,27.568388&amp;spn=0.005105,0.00912&amp;z=16" style="color:#0000FF;text-align:left">View Larger Map</a></small>
 				</div>
 				<div class="col-md-4">
-						<img alt="Bridge Club Iasi" class="img-thumbnail" data-src="holder.js/425x350" style="width: 425px; height: 350px;" src="./images/bridgeclub.jpg">
+						<img alt="Bridge Club Iasi" class="img-thumbnail" data-src="holder.js/425x350" style="width: 425px; height: 350px;" src="images/bridgeclub.jpg">
 				</div>
 				<div class="col-md-4">
 						<h2>Contact</h2>
 						<p>Adresa: Parcul Copou, Casa de Cultura "Mihai Ursache"</p>
-						<p>Telefon: 0730228476</p>
+						<p>Telefon: <a href="tel:0730228476">0730 228 476</a></p>
 						<p>E-mail: <a href="mailto:a1b2ff@yahoo.com">Stefan Iancu</a> </p>
 				</div>
 			</div>
 		</div>		
 	</div>
+
 
 	<?php include 'footer.php';?>
 	
