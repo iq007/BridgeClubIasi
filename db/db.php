94a8e88917c642230f92ad74fa1a17355ac8f96e
iq007 <a1b2ff@yahoo.com>
2015-03-11 09:27:13 +0200

    fixed path to db folder

--- db/db.php
+++ db/db.php
@@ -1,20 +1,26 @@
-<?require_once('./db/medoo.min.php');?>
+<?php
 
-<?
+require_once('./db/medoo.min.php');
+
+try{
     $database = new medoo([
-    // required
-    'database_type' => 'mysql',
-    'database_name' => getenv("DBNAME") ? getenv("DBNAME") : 'www_iasi_bridge_club_ro',
-    'server' => getenv("DBSERVER") ? getenv("DBSERVER") : 'localhost',
-    'username' => getenv("DBUSER") ? getenv("DBUSER") : 'root',
-    'password' => getenv("DBPASS") ? getenv("DBPASS") : '',
-    'charset' => 'utf8',
+        // required
+        'database_type' => 'mysql',
+        'database_name' => getenv("DBNAME") ? getenv("DBNAME") : 'www_iasi_bridge_club_ro',
+        'server' => getenv("DBSERVER") ? getenv("DBSERVER") : 'localhost',
+        'username' => getenv("DBUSER") ? getenv("DBUSER") : 'root',
+        'password' => getenv("DBPASS") ? getenv("DBPASS") : '',
+        'charset' => 'utf8',
 
-    // optional
-    'port' => 3306,
-    // driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
-    'option' => [
-        PDO::ATTR_CASE => PDO::CASE_NATURAL
-    ]
-]);
+        // optional
+        'port' => 3306,
+        // driver_option for connection, read more from http://www.php.net/manual/en/pdo.setattribute.php
+        'option' => [
+            PDO::ATTR_CASE => PDO::CASE_NATURAL
+        ]
+    ]);
+}
+catch(Exception $e){
+
+}
 ?>
